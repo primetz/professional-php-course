7458addb2e0a1437762e59edc8147b001fff8b2f
Stanislav Rodikov <stanislav.rodikov@yahoo.com>
2021-11-18 13:42:02 +0300

    Practical work 3

---
+++ hw_3/task_1/app/config/routes.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '/' => [
+        'controller' => 'gallery',
+        'action' => 'index',
+    ],
+    '/photo' => [
+        'controller' => 'gallery',
+        'action' => 'photo',
+    ],
+];
